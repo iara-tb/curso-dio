2184c9ff578b3e8e7ac4381614be589a5a2c00f5
Iara T.Bellodi <100525143+iara-tb@users.noreply.github.com>
2022-07-17 18:10:42 -0300

    Calculadora Aula Métodos
    Calculadora seguindo o passo a passo da Aula de Métodos

---
+++ CalculadoraMetodos.java
@@ -0,0 +1,22 @@
+
+public class CalculadoraMetodos {
+	///Seguindo passo a passo do exercício da Aula 1 de Métodos DIO 
+	
+	public static void soma(double numero1,double numero2) {
+		double resultado = numero1+numero2;
+		System.out.println("A soma de "+numero1+" mais "+numero2+ " é "+resultado);
+	}
+	public static void subtracao(double numero1,double numero2) {
+		double resultado = numero1-numero2;
+		System.out.println("A subtracao de "+numero1+" menos "+numero2+" é "+resultado);
+	}
+	public static void multiplicacao(double numero1,double numero2) {
+		double resultado = numero1+numero2;
+		System.out.println("A multiplicação de "+numero1+" multiplicado por "+numero2+" é "+resultado);
+	}
+	public static void divisao(double numero1,double numero2) {
+		double resultado = numero1+numero2;
+		System.out.println("A divisão de "+numero1+" por "+numero2+"é"+resultado);
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Exercício Calculadora");
+		CalculadoraMetodos.soma(3, 6);
+		CalculadoraMetodos.subtracao(9,1.8);
+		CalculadoraMetodos.multiplicacao(7,8);
+		CalculadoraMetodos.divisao(5, 2.5);
+
+	}
+
+}
